457553c954957cef3f42086f600e855f73d958b4
ArronZgk <113958940+ArronZgk@users.noreply.github.com>
2022-09-21 21:29:25 +0800

    pull commit

--- src/main/java/com/Git.java
+++ src/main/java/com/Git.java
@@ -6,6 +6,6 @@ public class Git {
         System.out.println("hello,git3");
         System.out.println("hello.hot-fix");
         System.out.println("hell0,git4");
-        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
