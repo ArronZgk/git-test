a16d316cdad349c87a7abb43c91938624f814055
Arron <Arron@zgk.com>
2022-09-21 20:42:49 +0800

    push commit

--- src/main/java/com/Git.java
+++ src/main/java/com/Git.java
@@ -6,8 +6,6 @@ public class Git {
         System.out.println("hello,git3");
         System.out.println("hello.hot-fix");
         System.out.println("hell0,git4");
-        System.out.println("i am master");
-        System.out.println("i hate hot-fix");
-        System.out.println("i am back hot-fix");
+        System.out.println("push test");
     }
 }
